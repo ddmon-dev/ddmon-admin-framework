7bf3e7f337d24f17e475d5c92462c89ce6020c07
dd2mon <ldhman91@gmail.com>
2025-11-22 23:04:21 +0900

    fix: supabase api key 변경

--- src/shared/lib/supabase/client.ts
+++ src/shared/lib/supabase/client.ts
@@ -8,6 +8,6 @@ import { Database } from './types';
 export function createBrowserClient() {
   return createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
   );
 }
