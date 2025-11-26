a151d43849dbaec61039f66829272fc1ca0d9b97
dd2mon <ldhman91@gmail.com>
2025-11-26 23:04:50 +0900

    refactor: manage-sheet 사용 구조 리팩토링

--- src/features/manage-modules/_template/item-sheet.tsx
+++ src/features/manage-modules/_template/item-sheet.tsx
@@ -1,25 +1,15 @@
 'use client';
 
-import { ManageSheet, useManageSheet } from '../_base/ui';
-import { useManageItemData } from '../_base/hooks';
+import { ManageSheet } from '../_base/ui';
 import { ItemForm } from './item-form';
 import { getItem } from './actions/get-item';
 
 export function ItemSheet() {
-  const manageSheet = useManageSheet();
-  const { id, mode } = manageSheet.data ?? {};
-  const { prevValues } = useManageItemData(getItem, {
-    additionalDateFields: ['birthDate'],
-  });
-
   return (
-    <ManageSheet>
-      {mode === 'view' ? null : (
-        <ItemForm
-          id={id}
-          prevValues={prevValues}
-        />
-      )}
-    </ManageSheet>
+    <ManageSheet
+      fetchFn={getItem}
+      additionalDateFields={['birthDate']}
+      formComponent={ItemForm}
+    />
   );
 }
