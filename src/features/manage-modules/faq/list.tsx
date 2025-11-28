2b76ac8b4f3b7045d7daf17fca8b6f118ffdedc3
dd2mon <ldhman91@gmail.com>
2025-11-29 02:44:03 +0900

    refactor: FAQ list 엑셀 다운로드 코드 제거

--- src/features/manage-modules/faq/list.tsx
+++ src/features/manage-modules/faq/list.tsx
@@ -14,45 +14,11 @@ interface ListProps {
 }
 
 export function List({ data, totalCount }: ListProps) {
-  // 엑셀 컬럼 정의
-  const excelColumns: ExcelColumn<ItemDTO>[] = [
-    {
-      header: '질문',
-      accessorKey: 'question',
-      width: 40,
-    },
-    {
-      header: '답변',
-      accessorKey: 'answer',
-      width: 15,
-    },
-    {
-      header: '작성일',
-      accessorFn: row => new Date(row.createdAt).toLocaleDateString(),
-      width: 15,
-    },
-  ];
-
   return (
-    <div className='space-y-4'>
-      {/* 엑셀 다운로드 버튼 */}
-      <div className='flex justify-end'>
-        <ExcelExportButton
-          fetchData={async () => {
-            return await getExportData();
-          }}
-          columns={excelColumns}
-          fileName={`데이터내보내기_${new Date().toLocaleDateString()}.xlsx`}
-          sheetName='Sheet1'
-        />
-      </div>
-
-      {/* 테이블 */}
-      <ManageList
-        data={data}
-        totalCount={totalCount}
-        listColumns={listColumns}
-      />
-    </div>
+    <ManageList
+      data={data}
+      totalCount={totalCount}
+      listColumns={listColumns}
+    />
   );
 }
