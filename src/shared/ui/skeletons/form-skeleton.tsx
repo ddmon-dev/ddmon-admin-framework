2886e3ae3d8a03c37131db12e37ab8770c5021b3
dd2mon <ldhman91@gmail.com>
2025-11-29 03:44:36 +0900

    style: Sheet 폼 레이아웃 및 Textarea 높이 개선
    - ManageSheet: form이 전체 높이를 채우고 footer 하단 고정
    - FormSkeleton: flex 레이아웃 적용, 불필요한 필드 제거
    - Textarea: 기본 최소 높이 150px로 증가

--- src/shared/ui/skeletons/form-skeleton.tsx
+++ src/shared/ui/skeletons/form-skeleton.tsx
@@ -2,13 +2,16 @@ import { Skeleton } from '@/shared/ui/skeleton';
 
 export function FormSkeleton() {
   return (
-    <div className='space-y-6 py-4'>
+    <div className='space-y-6 py-4 flex-1 flex flex-col'>
       {/* 라디오 그룹 */}
       <div className='space-y-3'>
         <Skeleton className='h-4 w-16' />
         <div className='flex gap-4'>
           {Array.from({ length: 3 }).map((_, i) => (
-            <div key={i} className='flex items-center gap-2'>
+            <div
+              key={i}
+              className='flex items-center gap-2'
+            >
               <Skeleton className='size-4 rounded-full' />
               <Skeleton className='h-4 w-12' />
             </div>
@@ -28,12 +31,6 @@ export function FormSkeleton() {
         <Skeleton className='h-10 w-32' />
       </div>
 
-      {/* 텍스트 입력 */}
-      <div className='space-y-2'>
-        <Skeleton className='h-4 w-10' />
-        <Skeleton className='h-10 w-full' />
-      </div>
-
       {/* 에디터 */}
       <div className='space-y-2'>
         <Skeleton className='h-4 w-10' />
@@ -43,17 +40,11 @@ export function FormSkeleton() {
       {/* 파일 업로드 (썸네일) */}
       <div className='space-y-2'>
         <Skeleton className='h-4 w-14' />
-        <Skeleton className='h-32 w-full rounded-lg' />
-      </div>
-
-      {/* 파일 업로드 (첨부파일) */}
-      <div className='space-y-2'>
-        <Skeleton className='h-4 w-16' />
-        <Skeleton className='h-32 w-full rounded-lg' />
+        <Skeleton className='h-10 w-full rounded-lg' />
       </div>
 
       {/* Footer */}
-      <div className='flex justify-end gap-2 pt-4 border-t'>
+      <div className='flex justify-end gap-2 pt-4 border-t mt-auto'>
         <Skeleton className='h-10 w-16' />
         <Skeleton className='h-10 w-16' />
       </div>
