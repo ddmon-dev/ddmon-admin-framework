3451eb5915ec035a40a727f323be94bff4e8d48a
dd2mon <ldhman91@gmail.com>
2025-12-01 21:21:20 +0900

    fix: 관리자 계정관리 불필요 기능 제거

--- src/features/manage-modules/admin/index.tsx
+++ src/features/manage-modules/admin/index.tsx
@@ -3,7 +3,6 @@ import { type SearchParams } from '@/shared/types/search-params';
 import { ManageContainer, ManageListFetcher } from '../_base/ui';
 
 import { CONFIG, type ItemDTO } from './config';
-import { HeaderAddons } from './addons';
 import { List } from './list';
 import { ItemSheet } from './item-sheet';
 
@@ -15,11 +14,17 @@ interface Props {
 
 export default function ManageModule({ searchParams }: Props) {
   return (
-    <ManageContainer moduleName={CONFIG.moduleName} headerAddons={<HeaderAddons />}>
-      <ManageListFetcher<ItemDTO> searchParams={searchParams} getList={getList}>
+    <ManageContainer moduleName={CONFIG.moduleName}>
+      <ManageListFetcher<ItemDTO>
+        searchParams={searchParams}
+        getList={getList}
+      >
         {({ data, totalCount }) => (
           <>
-            <List data={data} totalCount={totalCount} />
+            <List
+              data={data}
+              totalCount={totalCount}
+            />
             <ItemSheet />
           </>
         )}
