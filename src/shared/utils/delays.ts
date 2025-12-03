0d3bb8342a2bc4af6b8d7aaa6f7e224871823fe9
dd2mon <ldhman91@gmail.com>
2025-12-03 15:20:39 +0900

    fix: atLeast 유틸 타입 버그 수정

--- src/shared/utils/delays.ts
+++ src/shared/utils/delays.ts
@@ -19,7 +19,7 @@ export const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, 
  */
 export async function atLeast<T>(
   fn: () => Promise<T>,
-  minDuration = APP_CONFIG.UX.MIN_LOADING_TIME
+  minDuration: number = APP_CONFIG.UX.MIN_LOADING_TIME
 ): Promise<T> {
   const [result] = await Promise.all([fn(), delay(minDuration)]);
   return result;
