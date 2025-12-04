dba39d788c0d0961da37becbdb65efa6b11cd490
dd2mon <ldhman91@gmail.com>
2025-12-04 21:28:01 +0900

    fix: 스타일 수정

--- src/features/manage-modules/_base/ui/manage-sheet-error.tsx
+++ src/features/manage-modules/_base/ui/manage-sheet-error.tsx
@@ -13,32 +13,31 @@ export function ManageSheetError({ onRetry }: ManageSheetErrorProps) {
   const manageSheet = useManageSheet();
 
   return (
-    <Empty className='flex-1 border-0'>
-      <EmptyHeader>
-        <EmptyMedia className='size-16 rounded-full bg-destructive-light mb-4'>
-          <X
-            className='size-10 text-destructive'
-            strokeWidth={1}
-          />
-        </EmptyMedia>
-        <EmptyTitle className='text-xl'>데이터를 불러올 수 없습니다</EmptyTitle>
-        <EmptyDescription>
-          일시적인 오류가 발생했습니다.
-          <br />
-          잠시 후 다시 시도해 주세요.
-        </EmptyDescription>
-      </EmptyHeader>
+    <div className='flex-1 flex flex-col items-center justify-center'>
+      <Empty className='flex-0 border-destructive rounded-lg'>
+        <EmptyHeader>
+          <EmptyMedia className='size-16 rounded-full bg-destructive-light mb-4'>
+            <X className='size-10 text-destructive' />
+          </EmptyMedia>
+          <EmptyTitle className='text-4xl font-primary font-semibold'>ERROR!</EmptyTitle>
+          <EmptyDescription className='text-md mt-2'>
+            일시적인 오류가 발생했습니다.
+            <br />
+            잠시 후 다시 시도해 주세요.
+          </EmptyDescription>
+        </EmptyHeader>
 
-      <div className='flex gap-2'>
-        {onRetry && <Button onClick={onRetry}>다시 시도</Button>}
-        <Button
-          variant='destructive-light'
-          size='lg'
-          onClick={() => manageSheet.close()}
-        >
-          닫기
-        </Button>
-      </div>
-    </Empty>
+        <div className='flex gap-2'>
+          {onRetry && <Button onClick={onRetry}>다시 시도</Button>}
+          <Button
+            variant='black'
+            size='lg'
+            onClick={() => manageSheet.close()}
+          >
+            닫기
+          </Button>
+        </div>
+      </Empty>
+    </div>
   );
 }
